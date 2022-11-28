06132363c764b21e955b3e846f350055a81adf0c
p3wv <48331799+p3wv@users.noreply.github.com>
2022-11-28 15:43:34 +0100

    Update login.js

--- login.js
+++ login.js
@@ -1,13 +1,18 @@
+//utworzenie elementów na bazie id z login.html
 
 const loginForm = document.getElementById("login-form");
 const loginButton = document.getElementById("login-form-submit");
 const loginErrorMsg = document.getElementById("login-error-msg");
+
+//nasłuchiwanie kliknięcia na elementy username i password
 
 loginButton.addEventListener("click", (e) => {
     e.preventDefault();
     const username = loginForm.username.value;
     const password = loginForm.password.value;
 
+//warunek sprawdzający poprawność danych
+    
     if (username === "" && password === "") {
         alert("You have successfully logged in.");
         location.reload();
